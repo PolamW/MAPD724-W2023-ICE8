a5f9677c645673d1a7c2b9756155a57ef9396c0e
PolamW <pwong140@my.centennialcollege.ca>
2023-03-26 20:47:49 -0400

    done

---
+++ MAPD724-W2023-ICE8/RecognitionViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  MAPD724-W2023-ICE8
+//
+//  Created by Po Lam Wong on 26/3/2023.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class RecognitionViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var labelDescription: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let imagePath = Bundle.main.path(forResource: "kitten", ofType: "png")
+        let imageURL = NSURL.fileURL(withPath: imagePath!)
+        
+        let modelFile = MobileNetV2()
+        let model = try! VNCoreMLModel(for: modelFile.model)
+        
+        let handler = VNImageRequestHandler(url: imageURL)
+        let request = VNCoreMLRequest(model: model, completionHandler: findResults)
+        try! handler.perform([request])
+    }
+    
+    func findResults(request: VNRequest, error: Error?) {
+       guard let results = request.results as?
+       [VNClassificationObservation] else {
+       fatalError("Unable to get results")
+       }
+       var bestGuess = ""
+       var bestConfidence: VNConfidence = 0
+       for classification in results {
+          if (classification.confidence > bestConfidence) {
+             bestConfidence = classification.confidence
+             bestGuess = classification.identifier
+          }
+       }
+       labelDescription.text = "Image is: \(bestGuess) with confidence \(bestConfidence) out of 1"
+    }
+
+}
+
